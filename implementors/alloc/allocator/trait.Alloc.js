655f9b300929817a026f7826f3b16de520127dd8
Travis CI User <travis@example.org>
2017-11-05 17:56:27 +0000

    Documentation for alexcrichton/jemallocator@262a972

---
+++ implementors/alloc/allocator/trait.Alloc.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["jemallocator"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for <a class=\"struct\" href=\"jemallocator/struct.Jemalloc.html\" title=\"struct jemallocator::Jemalloc\">Jemalloc</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/allocator/trait.Alloc.html\" title=\"trait alloc::allocator::Alloc\">Alloc</a> for &amp;'a <a class=\"struct\" href=\"jemallocator/struct.Jemalloc.html\" title=\"struct jemallocator::Jemalloc\">Jemalloc</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
